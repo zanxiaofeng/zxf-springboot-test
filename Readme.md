76091ae1e5b5f4e0b6f0a73e9434dc210ae454bd
davis <zanxiaofeng@163.com>
2022-11-06 10:19:05 +0800

    update

--- Readme.md
+++ Readme.md
@@ -1,1 +1,23 @@
-# 
+# Bean Mock
+- @MockBean
+- @SpyBean
+
+# 集成测试
+## 启用
+- 集成测试使用@SpringBootTest注解来加载所有的bean，　使用＠ＭockＢean或＠ＳpyＢean来替换指定的Ｂean，使用Mock或Running Server并配合相应的工具类完成测试
+
+## Test Running Web server by WebTestClient or TestRestTemplate
+- @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+- @Autowired WebTestClient webClient(This setup requires spring-webflux on the classpath)
+- @Autowired TestRestTemplate restTemplate
+
+## Test Mock Web server by MockMvc
+- @SpringBootTest
+- @AutoConfigureMockMvc
+- @Autowired MockMvc
+
+
+# 切片测试
+## WebMvc切面测试
+- @WebMvcTest, 该注解仅会创建指定的Ｃontroller　bean，该Ｃontroller依赖的其他bean需要使用＠ＭockＢean注入
+
--- src/test/java/zxf/springboot/test/rest/HomeControllerTest.java
+++ src/test/java/zxf/springboot/test/rest/HomeControllerTest.java
@@ -3,20 +3,18 @@ package zxf.springboot.test.rest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HomeControllerTest {
     @Autowired
-    MockMvc mockMvc;
+    TestRestTemplate testRestTemplate;
 
     @Test
     void testGreeting() throws Exception {
