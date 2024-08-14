b61c9e0667ae5b8cf16ccf5c7953abf1dd70684c
Davis ZAN <zanxiaofeng@163.com>
2024-08-14 02:35:45 -0500

    update

--- zxf-springboot-ea-service/src/main/java/zxf/springboot/pa/client/PAClient.java
+++ zxf-springboot-ea-service/src/main/java/zxf/springboot/pa/client/PAClient.java
@@ -3,16 +3,14 @@ package zxf.springboot.pa.client;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.*;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -77,14 +75,94 @@ public class PAClient {
                 }
             });
         }
-        restTemplate.setInterceptors(List.of((request, body, execution) -> {
-            log.info("Request, method: {}, path: {}, headers: {}, body: {}", request.getMethod(), request.getURI(), request.getHeaders(), new String(body, StandardCharsets.UTF_8));
-            ClientHttpResponse response = execution.execute(request, body);
-//            InputStreamReader isr = new InputStreamReader(response.getBody(), StandardCharsets.UTF_8);
-//            String responseBody = new BufferedReader(isr).lines().collect(Collectors.joining("\n"));
-//            log.info("Response body: {}", responseBody);
-            return response;
-        }));
+        restTemplate.setInterceptors(List.of(new LoggingRequestInterceptor()));
         return restTemplate;
+    }
+
+    final static class LoggingRequestInterceptor implements ClientHttpRequestInterceptor {
+        @Override
+        public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+            logRequest(request, body);
+            BufferingClientHttpResponseWrapper response = new BufferingClientHttpResponseWrapper(execution.execute(request, body));
+            logResponse(response);
+            return response;
+        }
+
+        private void logRequest(HttpRequest request, byte[] body) {
+            System.out.println("=================================================Request being=================================================");
+            System.out.println("URI             : " + request.getURI());
+            System.out.println("Methed          : " + request.getMethod());
+            System.out.println("Headers         : " + request.getHeaders());
+            System.out.println("Request Body    : " + new String(body, StandardCharsets.UTF_8));
+            System.out.println("=================================================Request end=================================================");
+        }
+
+        private void logResponse(ClientHttpResponse response) throws IOException {
+            System.out.println("=================================================Response being=================================================");
+            System.out.println("Status code     : " + response.getStatusCode().value());
+            System.out.println("Status text     : " + response.getStatusCode().name());
+            System.out.println("Headers         : " + response.getHeaders());
+            System.out.println("Response Body   : " + toString(response));
+            System.out.println("=================================================Response end=================================================");
+        }
+
+        private String toString(ClientHttpResponse response) throws IOException {
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getBody(), StandardCharsets.UTF_8))) {
+                String inputStr;
+                StringBuilder builder = new StringBuilder();
+                while ((inputStr = bufferedReader.readLine()) != null) {
+                    builder.append(inputStr);
+                }
+                return builder.toString();
+            }
+        }
+    }
+
+    final static class BufferingClientHttpResponseWrapper implements ClientHttpResponse {
+
+        private final ClientHttpResponse response;
+
+        @Nullable
+        private byte[] body;
+
+
+        BufferingClientHttpResponseWrapper(ClientHttpResponse response) {
+            this.response = response;
+        }
+
+
+        @Override
+        public HttpStatus getStatusCode() throws IOException {
+            return this.response.getStatusCode();
+        }
+
+        @Override
+        public int getRawStatusCode() throws IOException {
+            return this.response.getRawStatusCode();
+        }
+
+        @Override
+        public String getStatusText() throws IOException {
+            return this.response.getStatusText();
+        }
+
+        @Override
+        public HttpHeaders getHeaders() {
+            return this.response.getHeaders();
+        }
+
+        @Override
+        public InputStream getBody() throws IOException {
+            if (this.body == null) {
+                this.body = StreamUtils.copyToByteArray(this.response.getBody());
+            }
+            return new ByteArrayInputStream(this.body);
+        }
+
+        @Override
+        public void close() {
+            this.response.close();
+        }
+
     }
 }
